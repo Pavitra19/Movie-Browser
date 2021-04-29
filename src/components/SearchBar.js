c27e5dc9b5d07af946f21f64afc6bd419fb1758c
Pavitra Kurseja <pavitra.kurseja@gmail.com>
2021-04-28 21:19:12 -0400

    onChange for searchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import InputAdornment from "@material-ui/core/InputAdornment";
 import FormControl from "@material-ui/core/FormControl";
 import InputLabel from "@material-ui/core/InputLabel";
@@ -6,12 +6,20 @@ import Input from "@material-ui/core/Input";
 import SearchIcon from "@material-ui/icons/Search";
 
 export default function SearchBar() {
+  const [movieName, setMovieName] = useState("");
+
+  const handleChange = (event) => {
+    setMovieName(event.target.value);
+  };
+
   return (
     <div>
       <FormControl>
         <InputLabel htmlFor="input-with-icon-adornment">Search</InputLabel>
         <Input
           id="input-with-icon-adornment"
+          onChange={handleChange}
+          value={movieName}
           startAdornment={
             <InputAdornment position="start">
               <SearchIcon />
