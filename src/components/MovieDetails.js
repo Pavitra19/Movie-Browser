b31a53f35a0dc2fd5a5729e577b880420a46337c
Pavitra Kurseja <pavitra.kurseja@gmail.com>
2021-04-30 15:53:47 -0400

    Fixed bug with notifs

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -1,21 +1,8 @@
 import React from "react";
 import MoviePreview from "./MoviePreview";
 import NominateButton from "./NominateButton";
-import Alert from "@material-ui/lab/Alert";
-import { makeStyles } from "@material-ui/core/styles";
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    width: "100%",
-    "& > * + *": {
-      marginTop: theme.spacing(2),
-    },
-  },
-}));
 
 export default function MovieDetails(props) {
-  const classes = useStyles();
-
   return (
     <div>
       <MoviePreview Title={props.Title} img={props.img} alt={props.Title} />
@@ -34,13 +21,6 @@ export default function MovieDetails(props) {
           )
         }
       />
-      <div className={classes.root}>
-        {props.favs.length === 5 && (
-          <Alert variant="filled" severity="warning">
-            You've reached your max of 5 favourites!
-          </Alert>
-        )}
-      </div>
     </div>
   );
 }
--- src/components/NominateButton.js
+++ src/components/NominateButton.js
@@ -1,9 +1,30 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Button from "@material-ui/core/Button";
 import FavoriteIcon from "@material-ui/icons/Favorite";
 import { makeStyles } from "@material-ui/core/styles";
+import Snackbar from "@material-ui/core/Snackbar";
+import MuiAlert from "@material-ui/lab/Alert";
+
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     width: "100%",
+//     "& > * + *": {
+//       marginTop: theme.spacing(2),
+//     },
+//   },
+// }));
 
 const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+    "& > * + *": {
+      marginTop: theme.spacing(2),
+    },
+  },
   button: {
     margin: theme.spacing(1),
     "&.Mui-disabled": {
@@ -17,7 +38,16 @@ export default function NominateButton(props) {
   let detailsURL = `http://www.omdbapi.com/?apikey=d66f3ecf&i=`;
   const [genre, setGenre] = useState();
   const [plot, setPlot] = useState();
+  const [open, setOpen] = useState(false);
   const classes = useStyles();
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleClick = () => {
+    setOpen(true);
+  };
 
   const getDetails = () => {
     detailsURL += `${props.imdbID}`;
@@ -35,12 +65,24 @@ export default function NominateButton(props) {
     <div>
       <h1>{genre}</h1>
       <p>{plot}</p>
+      <div className={classes.root}>
+        {props.favs.length === 2 && (
+          <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+            <Alert onClose={handleClose} variant="filled" severity="warning">
+              You've reached your max of 5 favourites!
+            </Alert>
+          </Snackbar>
+        )}
+      </div>
       <Button
         variant="contained"
         color="secondary"
         className={classes.button}
         endIcon={<FavoriteIcon />}
-        onClick={props.handleClick}
+        onClick={() => {
+          props.handleClick();
+          handleClick();
+        }}
         disabled={props.favs.length >= 5}
       >
         Favourite
