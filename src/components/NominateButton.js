b9a23df263807af3711e144f6a3c16b2cae12eee
Pavitra Kurseja <pavitra.kurseja@gmail.com>
2021-05-01 13:37:16 -0400

    Added table with more data; fixed overflow problem

--- src/components/NominateButton.js
+++ src/components/NominateButton.js
@@ -1,22 +1,18 @@
 import React, { useEffect, useState } from "react";
 import Button from "@material-ui/core/Button";
 import FavoriteIcon from "@material-ui/icons/Favorite";
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
 import Snackbar from "@material-ui/core/Snackbar";
 import MuiAlert from "@material-ui/lab/Alert";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableRow from "@material-ui/core/TableRow";
 
 function Alert(props) {
   return <MuiAlert elevation={6} variant="filled" {...props} />;
 }
-
-// const useStyles = makeStyles((theme) => ({
-//   root: {
-//     width: "100%",
-//     "& > * + *": {
-//       marginTop: theme.spacing(2),
-//     },
-//   },
-// }));
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -34,8 +30,25 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const StyledTableCell = withStyles((theme) => ({
+  body: {
+    fontSize: 14,
+    color: "white",
+  },
+}))(TableCell);
+
+const StyledTableRow = withStyles((theme) => ({
+  root: {
+    backgroundColor: "pink",
+    "&:nth-of-type(odd)": {
+      backgroundColor: "grey",
+    },
+  },
+}))(TableRow);
+
 export default function NominateButton(props) {
   let detailsURL = `http://www.omdbapi.com/?apikey=d66f3ecf&i=`;
+  const [movieDetails, setMovieDetails] = useState({});
   const [genre, setGenre] = useState();
   const [plot, setPlot] = useState();
   const [open, setOpen] = useState(false);
@@ -53,18 +66,56 @@ export default function NominateButton(props) {
     detailsURL += `${props.imdbID}`;
     fetch(detailsURL).then(async (details) => {
       details = await details.json();
-      // console.log("details: ", details);
-      setGenre(details.Genre);
-      setPlot(details.Plot);
+      console.log("details: ", details);
+      setMovieDetails((prevValues) => ({
+        ...prevValues,
+        Genre: details.Genre,
+        Plot: details.Plot,
+        Actors: details.Actors,
+        Awards: details.Awards,
+        imdbRating: details.imdbRating,
+      }));
+      // setGenre(details.Genre);
+      // setPlot(details.Plot);
     });
   };
 
   getDetails();
 
+  function createData(genre, plot, actors, awards) {
+    return { genre, plot, actors, awards };
+  }
+
+  // Object.keys(movieDetails);
+
+  const rows = [
+    createData("Genre", movieDetails.Genre),
+    createData("Plot", movieDetails.Plot),
+    createData("Actors", movieDetails.Actors),
+    createData("Awards", movieDetails.Awards),
+  ];
+
   return (
     <div>
-      <h1>{genre}</h1>
-      <p>{plot}</p>
+      <TableContainer>
+        <Table className={classes.table} aria-label="simple table">
+          <TableBody>
+            {rows.map((row) => (
+              <StyledTableRow key={row.name}>
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <StyledTableCell align="center">{row.genre}</StyledTableCell>
+                <StyledTableCell align="center">{row.plot}</StyledTableCell>
+                <StyledTableCell align="center">{row.actors}</StyledTableCell>
+                <StyledTableCell align="center">{row.awards}</StyledTableCell>
+              </StyledTableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      {/* <h1>Genre: {genre}</h1>
+      <p> Plot: {plot}</p> */}
       <div className={classes.root}>
         {props.favs.length === 2 && (
           <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -44,7 +44,7 @@ const useStyles = makeStyles((theme) => ({
     flexGrow: 1,
     // backgroundColor: theme.palette.background.paper,
     display: "flex",
-    height: 550,
+    height: 650,
     marginTop: "43px",
   },
   tabs: {
